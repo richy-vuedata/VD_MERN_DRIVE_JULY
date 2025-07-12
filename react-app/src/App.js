b73008f5fdffd6586502e2b9573bed6b026f2404
Vigneshwar Ram <vigneshwarram.mb@vuedata.in>
2025-07-12 14:28:54 +0530

    Fetch & Display data in table

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from 'react'
 
 function App() {
+const [data,setData] = useState([])
+const fetchApi=async()=>{
+  try{
+const fetchUrl = await fetch('https://reqres.in/api/users?page=2')
+  
+  const result =await fetchUrl.json()  
+    setData(result?.data)
+    console.log({result:result.data});
+
+  return result
+
+  }catch(error){
+    console.log(error);
+    
+  }
+}
+
+console.log({data:data?.map((data)=>data)});
+
+useEffect(()=>{
+  const value =fetchApi()
+},[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <table> */}
+        {data?.map((data)=>(
+          <li>
+            <ul>{data?.id}</ul>
+            <ul>{data?.email}</ul>
+
+            </li>
+        ))}
+      {/* </table> */}
     </div>
   );
 }
