7817a90f3e741955515f524484f2d4770efeec31
Vigneshwar Ram <vigneshwarram.mb@vuedata.in>
2025-07-12 16:01:33 +0530

    Fetch users display & render it on table

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState(null);
+
+  console.log(users);
+
+  const fetchUsers = async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+        method: "GET"
+      });
+      const data = await res.json();
+    
+      if (res.status === 200) {
+        setUsers(data);
+      } else {
+        console.log(res?.error);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers(); 
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <th>User Name</th>
+        <th>Email</th>
+        {
+        users && users?.length === 0 ? <span>No users found</span> : users?.map((user) => (
+          <tr>
+           <td>{user.name}</td>
+           <td>{user.email}</td>
+          </tr>
+        ))  
+        }
+      </table>
+      
     </div>
   );
 }
