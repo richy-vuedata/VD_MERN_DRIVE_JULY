ae610ed1617b0f1da0927a0125c4bfd98fdede5f
Vigneshwar Ram <vigneshwarram.mb@vuedata.in>
2025-07-12 16:02:00 +0530

    API to return list of users with partial search

--- node-express/index.js
+++ node-express/index.js
@@ -4,7 +4,41 @@ const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-})
+});
+
+const getAllUsers = (req, res) => {
+    const { name } = req.query;
+
+    if (!name) {
+        res.status(400).json({ message: "name is required" });
+        return;
+    }
+    
+    const regex = new RegExp(name, 'i').test(name);
+
+    if (!regex) {
+        res.status(404).json({ message: "user not found" });
+        return;
+    }
+
+    const users = [
+       {
+        name: "subash",
+        email: "suabsh@gmail.com"
+       },
+       {
+        name: "vignesh",
+        email: "suabsh@gmail.com"
+       }
+    ];
+
+    const filteredUsers = users.filter(user => user.name.toLowerCase() === name.toLowerCase());
+
+    return res.status(200).json({ message: "success", filteredUsers});
+ 
+}
+
+app.get('/users', getAllUsers);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
