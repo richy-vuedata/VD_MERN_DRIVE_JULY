ee60bd204076f75a62aaa6fcc91e06d0cfe36a2d
Muthu <muthu.n@vuedata.in>
2025-07-12 15:12:10 +0530

    SD272

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <p>Check</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+// import { ButtonCounter,useState } from './components/ButtonCounter';
+import { useState } from 'react';
+
+const items = [
+  { title: '1', content: 'First content' },
+  { title: '2', content: 'Second content' },
+];
+
+export const App = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleClick = (index) => {
+    console.log('index', index);
+    setActiveIndex(index === activeIndex ? null : index);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {items.map((item, index) => (
+        <div>
+          <button key={index} onClick={() => handleClick(index)}>
+            {item.title}
+          </button>
+          {activeIndex === index && <div>{item.content}</div>}
+        </div>
+      ))}
     </div>
   );
-}
+};
 
-export default App;
