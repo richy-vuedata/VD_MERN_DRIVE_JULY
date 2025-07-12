7b78fe234a6e30d043472934ef261f8d73aa75ef
siva <sivasankaran.s@vuedata.in>
2025-07-12 05:52:14 -0700

    update

--- node-express/index.js
+++ node-express/index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+// select * from product_table('product_id',"product_name","price") where(price > 10 and price < 50)
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 })
+
+function anagrams(str1,str2){
+    if(str1 ){
+        return 'true'
+    }else{
+        return 'false'
+    }
+}
+anagrams('listen','silent');
+anagrams('hello','world');
