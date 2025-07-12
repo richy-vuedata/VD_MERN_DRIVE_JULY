90bd523499aec5b28797a0f4445a6a0a0e3b74f9
siva <sivasankaran.s@vuedata.in>
2025-07-11 21:54:12 -0700

    update

--- node-express/index.js
+++ node-express/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+})
+
+app.getUsers('/users', (res) => {
+       res.send([{
+        userName: 'Surya',
+        userId: '01',
+    },{
+        userName: 'Deepak',
+        userId: '02'
+    }])
 })
 
 app.listen(port, () => {
