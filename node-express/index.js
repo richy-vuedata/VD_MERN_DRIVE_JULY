6cddfa9b7483dcd9f46dd126568ea816ba0b2438
Vigneshwar Ram <vigneshwarram.mb@vuedata.in>
2025-07-12 18:49:45 +0530

    User API wirh Pagination

--- node-express/index.js
+++ node-express/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const usersData = require("./usersData")
+// const usersData = ... data
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    const {skip, limits} = req.query // coming from params of the request
+
+    // removing the values using splice from usersData    
+    // trying to consoling the first 20 users in usersData
+    const firstValues = usersData.slice(skip, limits)
+    console.log(firstValues)
+    res.send(firstValues);
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}  `);
 })
