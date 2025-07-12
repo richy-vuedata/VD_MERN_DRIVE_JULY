1cd76adcfe30b3d639507910cb44207a39208a8a
benny <ad@example.com>
2025-07-12 15:57:12 +0530

    sohel

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,12 +3,39 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
+
+  // const [data, setData] = useState([])
+  const [value, setValue] = useState([])
+
   
+  // useEffect( async () => {
+  //   let timerOut 
+
+  // })
+
+  // const handleData = async () => {
+  //   console.log("Pirate")
+  //   const response = await axios.get("https://fakestoreapi.com/products")
+  //   const data = response.json
+  //   setData(data)
+  // }
+
   return (
     <div className="App">
-      <p>test</p>
+      <p>Pirate</p>
+      {/* <button onClick={handleData}>Get Data</button>
+      {data ? 
+      <div>
+        {data.map((each) => {
+          <h1>{each.title}</h1>
+        })}
+      </div> 
+      
+      : null} */}
+      <input type='text' value={(e) => e.value} />
     </div>
   );
 }
 
 export default App;
+
