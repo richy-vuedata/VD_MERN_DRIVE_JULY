11f26689a016e029b3f621aac4658df620da2b2b
karthikeyankalathiS <karthikeyankalathi.s@vuedata.in>
2025-07-12 15:43:57 +0530

    Fetch data from the API using fetch

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-import Accordian from './Accordian';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => setData(response.json))
+    .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <Accordian/>
+      {data ? data.map((index, detail) => {
+        <table>
+          <thead>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+          </thead>
+          <tbody>
+            <tr key={index}>
+              <td>{detail.name}</td>
+              <td>{detail.email}</td>
+              <td>{detail.address.city}</td>
+            </tr>
+          </tbody>
+        </table>
+      }) : <p>Data is Loading</p>}
     </div>
   );
 }
