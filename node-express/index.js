eb75c503366b71d866b964a3b1cfe8eaa6132169
Muthu <muthu.n@vuedata.in>
2025-07-12 16:48:11 +0530

    WI19

--- node-express/index.js
+++ node-express/index.js
@@ -1,38 +1,55 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+// const express = require('express');
+// const app = express();
+// const port = 3000;
 
 
-let users=[{user_id:1,user_name:"nassaar"},{user_id:2,user_name:"mohamed"},{user_id:3,user_name:"manoj"},{user_id:4,user_name:"adnan"}]
+// let users=[{user_id:1,user_name:"nassaar"},{user_id:2,user_name:"mohamed"},{user_id:3,user_name:"manoj"},{user_id:4,user_name:"adnan"}]
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// })
 
-app.get('/users', (req, res) => {
-    res.send({data:users});
-})
-app.post('/users', (req, res) => {
-  let userDetails=req.body
-  users.push(userDetails)
-    res.send({msg:"success"});
-})
-app.put('/users', (req, res) => {
-  let user_name=req.body.user_name
-  users.push(userDetails)
-    res.send({msg:"updated"});
-})
-app.delete('/users/:id', (req, res) => {
-  let user_id=req.params.id
-  users.filter(data=>data.user_id===user_id)
-    res.send({msg:"deleted"});
-})
-app.get('/users/:id', (req, res) => {
-  let user_id=req.params.id
- let data= users.filter(data=>data.user_id===user_id)
-    res.send({data:data});
-})
+// app.get('/users', (req, res) => {
+//     res.send({data:users});
+// })
+// app.post('/users', (req, res) => {
+//   let userDetails=req.body
+//   users.push(userDetails)
+//     res.send({msg:"success"});
+// })
+// app.put('/users', (req, res) => {
+//   let user_name=req.body.user_name
+//   users.push(userDetails)
+//     res.send({msg:"updated"});
+// })
+// app.delete('/users/:id', (req, res) => {
+//   let user_id=req.params.id
+//   users.filter(data=>data.user_id===user_id)
+//     res.send({msg:"deleted"});
+// })
+// app.get('/users/:id', (req, res) => {
+//   let user_id=req.params.id
+//  let data= users.filter(data=>data.user_id===user_id)
+//     res.send({data:data});
+// })
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-})
+// app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// })
+
+
+
+
+function debounce(func, delay) {
+  let timer; 
+  return function(...args) {
+    clearTimeout(timer);  
+    timer = setTimeout(() => func(...args), delay);  
+  };
+}
+
+const searchInput = document.querySelector('#searchinput');
+const handleSearch = debounce((event) => {
+  console.log('Searching for:', event.target.value);  
+}, 100);  
+
