154a75af3a3c781823cd9efba51a7ff036901065
karthikeyankalathiS <karthikeyankalathi.s@vuedata.in>
2025-07-12 12:40:39 +0530

    Write a function to check if two strings are anagrams of each other

---
+++ react-app/src/program.js
@@ -0,0 +1,29 @@
+let word1='listen'
+let word2 ='silent'
+
+let arr=[]
+
+arr.push(word2)
+let finalArr
+
+word1 = word1.split('').sort().join('');
+word2 = word2.split('').sort().join('');
+
+if(word1.length===word2.length){
+// for(let i=0;i<word1.length;i++){
+//   for(let j=0;j<word2.length;j++){
+//     if(word1(i)===word2(j)){
+//       finalArr.push(word1(i))
+//     }
+//   }
+  
+// }
+if(word1===word2){
+  console.log(true)
+}
+
+else{
+  console.log(false)
+}
+}
+
